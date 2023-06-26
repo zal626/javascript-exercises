f127b57129924140bba1a145fdc0afc9e8b74c9f
zal626 <97076128+zal626@users.noreply.github.com>
2023-06-26 14:26:01 +0800

    Finish task 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(...num) {
+  let a = num[0];
+  let b = num[1];
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(...num) {
+  let a = num[0];
+  let b = num[1];
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(num) {
+  return num.reduce((s, current) => s += current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(...num) {
+  return num.reduce((a, b) => a *= b);
 };
 
-const power = function() {
-	
+const power = function(...num) {
+	let a = num[0];
+  let b = num[1];
+  return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0 || num === 1) return 1;
+  for (let i = num - 1; i > 1; i--){
+    num *= i;
+  }
+  return num
 };
 
 // Do not edit below this line
