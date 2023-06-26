689883a12c96b0c2320a793b5e5b42831cb268ad
zal626 <97076128+zal626@users.noreply.github.com>
2023-06-26 14:49:39 +0800

    Finish task 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let newStr = string.replace(/\W/g, '').toLowerCase();
+    let palindrome =  newStr.split('').reverse().join('');
+    return palindrome === newStr;
 };
 
 // Do not edit below this line
