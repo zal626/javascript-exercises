6f58413ac222675e83343f493ed0576a3f2a4954
zal626 <97076128+zal626@users.noreply.github.com>
2023-06-26 15:16:16 +0800

    Finish task 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let num1 = parseInt(num);
+    if (num1 < 0) return "OOPS"
+    let sum = 0;
+    let fib = [0, 1];
+    for (let i = 2; i <= num1; i++){
+        sum = fib[i-2] + fib[i-1];
+        fib.push(sum);
+    }
+    return fib[num1];
 };
 
 // Do not edit below this line
