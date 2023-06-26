7bfe7cc045bd0005c9c90d774027afe13a123b05
zal626 <97076128+zal626@users.noreply.github.com>
2023-06-26 16:55:16 +0800

    Finish task 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, current) => {
+        let a = getAge(oldest["yearOfBirth"],oldest["yearOfDeath"]);
+        let b = getAge(current["yearOfBirth"],current["yearOfDeath"]);
+        return a > b ? oldest: current;
+    });
 };
+
+function getAge(birth, death){
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
